f7d962ffb7b0837025f27e536dfa11d5f0fd255b
SimonBarilaro <162722001+SimonBarilaro@users.noreply.github.com>
2024-03-22 12:28:48 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,15 @@
 def change():
     expense = 23.75
     money = 100
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print("\n" + "Vuelto")
+    print("\n" + "Pesos:")
+    print(int(money - expense))
+    print("Centavos:")
+    vuelto_real=(float(money - expense))
+    centavos= ((vuelto_real - int(money - expense))*100)
+    print(int(centavos))
+change()
